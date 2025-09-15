1a29748a71889cf47f281bd4da993231d832f108
Bianca <ghostekkjk@gmail.com>
2025-09-15 09:54:12 -0300

    finalização do basico da atividade

--- src/controllers/carrosController.js
+++ src/controllers/carrosController.js
@@ -22,4 +22,102 @@ const getCarroById = (req, res) => {
     });
 };
 
-export { getAllCarros, getCarroById }
+const createCarro = (req, res) => {
+    const { nome, modelo, ano, cor, qtdeVitorias } = req.body;
+
+    if (!nome || !modelo || !ano || !cor || !qtdeVitorias) {
+        return res.status(400).json({
+            sucess: false,
+            message: "Nome, modelo, ano, cor e quantia de vitórias são obrigatórios."
+        });
+    };
+
+    const novoCarro = {
+        id: carros.length +1,
+        nome: nome,
+        modelo: modelo,
+        ano: parseInt(ano),
+        cor: cor,
+        qtdeVitorias: parseInt(qtdeVitorias)
+    };
+
+    carros.push(novoCarro);
+
+    res.status(201).json({
+        sucess: true,
+        message: "Barbie criada com sucesso",
+        Barbie: novoCarro
+    });
+};
+
+const deleteCarro = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)){
+        return res.status(400).json({
+            sucess: false,
+            message: "O id é inválido."
+        });
+    };
+
+    const carroParaRemover = carros.find(r => r.id === id);
+
+    if (!carroParaRemover) {
+        return res.status(404).json({
+            sucess: false,
+            message: "O carro com esse id não existe."
+        });
+    };
+
+    const carrosFiltrados = carros.filter(c => c.id != id);
+
+    carros.splice(0, carros.length, ...carrosFiltrados);
+
+    res.status(200).json({
+        sucess: true,
+        message: "O carro foi deletado."
+    });
+};
+
+const updateCarro = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nome, modelo, ano, cor, qtdeVitorias } = req.body;
+    const idParaEditar = id;
+
+    if(isNaN(idParaEditar)) {
+        return res.status(400).json({
+            sucess: false,
+            message: "O id deve ser válido."
+        });
+    };
+
+    const carroExiste = carros.find(c => c.id === idParaEditar);
+    if(!carroExiste) {
+        return res.status(404).json({
+            sucess: false,
+            message: "Esse carro não existe"
+        });
+    };
+
+    const carrosAtualizados = carros.map(c => c.id === idParaEditar ?{
+        ...c,
+        ...(nome && { nome }),
+        ...(modelo && { modelo }),
+        ...(ano && { ano: parseInt(ano) }),
+        ...(cor && { cor }),
+        ...(qtdeVitorias && { qtdeVitorias: parseInt(qtdeVitorias) })
+    }
+        :c
+    );
+
+    carros.splice(0, carros.length, ...carrosAtualizados);
+
+    const carroEditado = carros.find(c => c.id === idParaEditar);
+    res.status(200).json({
+        sucess: true,
+        message: "Dados atualizados com sucesso",
+        Carro: carroEditado
+    });
+};
+
+export { getAllCarros, getCarroById, createCarro, deleteCarro, updateCarro }
--- src/routes/carrosRoutes.js
+++ src/routes/carrosRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAllCarros, getCarroById } from "../controllers/carrosController.js";
+import { createCarro, deleteCarro, getAllCarros, getCarroById, updateCarro } from "../controllers/carrosController.js";
 
 const router = express.Router();
 
 router.get("/", getAllCarros);
 router.get("/:id", getCarroById);
+router.post("/", createCarro);
+router.delete("/:id", deleteCarro);
+router.put("/:id", updateCarro);
 
 export default router;
